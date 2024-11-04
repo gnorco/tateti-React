93a370eb059499640983f25411dfb212983557e3
gnorco <gnardironco@escuelasproa.edu.ar>
2024-11-04 14:50:47 -0300

    agregando estilos y funcionamiento basico de la pagina

--- tateti/src/App.jsx
+++ tateti/src/App.jsx
@@ -1,11 +1,22 @@
 import { useState } from 'react'
 
 function App() {
-
+  const [cells,setCells] = useState([null,null,null,null,null,null,null,null,null])
 
   return (
     <>
-     
+      <h1 style={{textAlign:"center",marginTop : "50px"}}>Tateti</h1>
+      <div className="container">
+        <div value="0" onClick={pressed} className="item"></div>
+        <div value="1" onClick={pressed} className="item"></div>
+        <div value="2" onClick={pressed} className="item"></div>
+        <div value="3" onClick={pressed} className="item"></div>
+        <div value="4" onClick={pressed} className="item"></div>
+        <div value="5" onClick={pressed} className="item"></div>
+        <div value="6" onClick={pressed} className="item"></div>
+        <div value="7" onClick={pressed} className="item"></div>
+        <div value="8" onClick={pressed} className="item"></div>
+      </div>
     </>
   )
 }
